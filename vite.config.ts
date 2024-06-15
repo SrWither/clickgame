6ced8b5d5f099266659902aa8fcc2e91c8e3d053
Fadel <59105868+SrWither@users.noreply.github.com>
2024-06-15 20:18:45 -0300

    base for production fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/clickgame/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
